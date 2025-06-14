6c652ee8694c9f7330c19c3fd173ffc4ec5ad4b6
Federico Sanchez <fede14.sanchez@gmail.com>
2025-06-14 20:50:10 -0300

    Add authentication to header

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,16 +1,24 @@
+import useAuthentication from '../../hooks/useAuthentication';
 import './header.css'
 
 function Header() {
+
+  const { isAuthenticated } = useAuthentication()
+
   return (
     <header className='header'>
-      <div>
-        <a href='/home'>Listado de alumnos</a>
-        <a href='/cuotas'>Cuotas mensuales</a>
-      </div>
-      <div>
-        <a href='/nuevo-alumno'>Nuevo alumno</a>
-        <a href='/login'>Salir</a>
-      </div>
+      { isAuthenticated && (
+        <>
+        <div>
+          <a href='/home'>Listado de alumnos</a>
+          <a href='/cuotas'>Cuotas mensuales</a>
+        </div>
+        <div>
+          <a href='/nuevo-alumno'>Nuevo alumno</a>
+          <a href='/login'>Salir</a>
+        </div>
+        </>
+      )}
     </header>
   )
 }
