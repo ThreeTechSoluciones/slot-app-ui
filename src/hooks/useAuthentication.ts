e6bf668013b3187a4c7ef7125c9f8a080ad49a9e
Federico Sanchez <fede14.sanchez@gmail.com>
2025-06-15 17:05:11 -0300

    [REF][SLOT-13] Refactor due to code review

--- src/hooks/useAuthentication.ts
+++ src/hooks/useAuthentication.ts
@@ -2,8 +2,6 @@ import { useSigninMutation } from '../app/services/AuthService';
 
 const useAuthentication = () => {
   const [ , { data } ] = useSigninMutation({ fixedCacheKey: 'shared-auth' })
-  console.log('signinData', data)
-  console.log('useAuth', { isAuthenticated: data?.accessToken != null, userId: data?.userId } )
   return { isAuthenticated: data?.accessToken != null, userId: data?.userId } 
 }
 
