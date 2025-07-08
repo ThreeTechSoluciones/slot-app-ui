5cc6347fde3dd65d5e65c1f5e1730bad29d72c8b
RocioMenardi <romenardi08@gmail.com>
2025-07-08 10:02:13 -0300

    [REF][SLOT-15] Add corrections from code review

--- src/pages/students/Students.tsx
+++ src/pages/students/Students.tsx
@@ -1,33 +1,27 @@
 import "./Students.css";
 import { useForm } from "react-hook-form";
-import type { Resolver } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { studentsScheme } from "../students/students.scheme";
-import { MOCKED_PAYMENT_TYPES } from "../../mocks/paymentTypes";
-import { useNavigate } from "react-router-dom";
-import ErrorIcon from "../../assets/error-icon.png";
+import { useNavigate } from "react-router";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
 import { Controller } from "react-hook-form";
+import { ErrorMessage } from "../../components/header/ErrorMessage";
+import { PaymentType } from "../../app/types/responses/Enum";
 
 type FormData = {
   name: string;
   lastname: string;
-  nroTelefono: string;
-  fechaNacimiento: Date | null;
-  tipoPago: string;
-  cantidadDias: number;
-  diaPago: string;
-  diasExtra: string;
-  fechaIngreso: Date | null;
+  phoneNumber: string;
+  birthday: Date;
+  paymentType: string;
+  classesPerWeek: number;
+  addmisionDate: Date;
+  payDay: string;
+  extraDays: string;
 };
 function Students() {
   const navigate = useNavigate();
-  const resolver = yupResolver(studentsScheme) as Resolver<FormData>;
-
-  const onSubmit = (data: any) => {
-    console.log("Formulario enviado con datos:", data);
-  };
   const {
     register,
     handleSubmit,
@@ -35,57 +29,39 @@ function Students() {
     control,
     formState: { errors },
   } = useForm<FormData>({
-    resolver,
+    resolver: yupResolver(studentsScheme),
     defaultValues: {
-      name: "",
-      lastname: "",
-      nroTelefono: "",
-      fechaNacimiento: null,
-      tipoPago: "",
-      diaPago: "",
-      diasExtra: "",
-      fechaIngreso: null,
+      birthday: new Date(),
+      addmisionDate: new Date(),
     },
   });
-  const tipoPagoSeleccionado = watch("tipoPago");
+  const onSubmit = (data: FormData) => {
+    console.log("Formulario enviado con datos:", data);
+  };
+  const paymentTypeSelected = watch("paymentType");
   return (
     <form className="form-container" onSubmit={handleSubmit(onSubmit)}>
       <div className="form-group">
         <div className="column1">
           <input {...register("name")} placeholder="Nombre" />
-          {errors.name && (
-            <div className="error-container">
-              <img src={ErrorIcon} alt="Error" />
-              <p className="error-message">{errors.name.message}</p>
-            </div>
-          )}
+          <ErrorMessage error={errors.name} />
 
           <input {...register("lastname")} placeholder="Apellido" />
-          {errors.lastname && (
-            <div className="error-container">
-              <img src={ErrorIcon} alt="Error" />
-              <p className="error-message">{errors.lastname.message}</p>
-            </div>
-          )}
+          <ErrorMessage error={errors.lastname} />
 
           <input
-            {...register("nroTelefono")}
+            {...register("phoneNumber")}
             type="tel"
             placeholder="Nro Teléfono"
           />
-          {errors.nroTelefono && (
-            <div className="error-container">
-              <img src={ErrorIcon} alt="Error" />
-              <p className="error-message">{errors.nroTelefono.message}</p>
-            </div>
-          )}
+          <ErrorMessage error={errors.phoneNumber} />
 
           <Controller
-            name="fechaNacimiento"
+            name="birthday"
             control={control}
             render={({ field }) => (
               <DatePicker
-                placeholderText="Fecha Nacimiento"
+                placeholderText="Fecha de Nacimiento"
                 selected={field.value}
                 onChange={(date) => field.onChange(date)}
                 dateFormat="dd/MM/yyyy"
@@ -97,19 +73,14 @@ function Students() {
               />
             )}
           />
-          {errors.fechaNacimiento && (
-            <div className="error-container">
-              <img src={ErrorIcon} alt="Error" />
-              <p className="error-message">{errors.fechaNacimiento.message}</p>
-            </div>
-          )}
+          <ErrorMessage error={errors.birthday} />
           <input type="text" placeholder="Patologías/Enfermedades" />
         </div>
 
         <div className="column2">
           <div>
             <Controller
-              name="fechaIngreso"
+              name="addmisionDate"
               control={control}
               render={({ field }) => (
                 <DatePicker
@@ -120,57 +91,38 @@ function Students() {
                 />
               )}
             />
-            {errors.fechaIngreso && (
-              <div className="error-container">
-                <img src={ErrorIcon} alt="Error" />
-                <p className="error-message">{errors.fechaIngreso.message}</p>
-              </div>
-            )}
+            <ErrorMessage error={errors.addmisionDate} />
           </div>
 
-          <select className="tipoPago" {...register("tipoPago")}>
-            <option value="" disabled selected hidden>
+          <select className="paymentType" {...register("paymentType")}>
+            <option value="" disabled hidden>
               Tipo de Pago
             </option>
-            {MOCKED_PAYMENT_TYPES.map((tipopago) => (
-              <option key={tipopago.id} value={tipopago.name}>
-                {tipopago.name}
+            {Object.values(PaymentType).map((name) => (
+              <option key={name} value={name}>
+                {name}
               </option>
             ))}
           </select>
+          <ErrorMessage error={errors.paymentType} />
           <input
             type="number"
-            {...register("cantidadDias")}
+            {...register("classesPerWeek")}
             placeholder="Días a la Semana"
           />
-          {errors.cantidadDias && (
-            <div className="error-container">
-              <img src={ErrorIcon} alt="Error" />
-              <p>{errors.cantidadDias.message}</p>
-            </div>
-          )}
+          <ErrorMessage error={errors.classesPerWeek} />
           <input
             type="text"
             placeholder="Día de Pago"
-            disabled={tipoPagoSeleccionado !== "Día específico"}
+            disabled={paymentTypeSelected !== "Día específico"}
           />
-          {errors.diaPago && (
-            <div className="error-container">
-              <img src={ErrorIcon} alt="Error" />
-              <p>{errors.diaPago.message}</p>
-            </div>
-          )}
+          <ErrorMessage error={errors.payDay} />
           <input
             type="text"
             placeholder="Días Extras"
-            disabled={tipoPagoSeleccionado !== "Del 1 al 10"}
+            disabled={paymentTypeSelected !== "Del 1 al 10"}
           />
-          {errors.diasExtra && (
-            <div className="error-container">
-              <img src={ErrorIcon} alt="Error" />
-              <p>{errors.diasExtra.message}</p>
-            </div>
-          )}
+          <ErrorMessage error={errors.extraDays} />
         </div>
       </div>
       <div className="button-group">
@@ -178,7 +130,6 @@ function Students() {
           className="form-button cancelButton"
           onClick={() => navigate("/home")}
         >
-          {" "}
           Cancelar
         </button>
         <button className="form-button registerButton" type="submit">
--- src/pages/students/students.scheme.ts
+++ src/pages/students/students.scheme.ts
@@ -1,42 +1,46 @@
 import * as yup from "yup";
+import { PaymentType } from "../../app/types/responses/Enum";
 
 export const studentsScheme = yup
   .object({
     name: yup.string().required("Debe ingresar el nombre"),
     lastname: yup.string().required("Debe ingresar el apellido"),
-    nroTelefono: yup
+    phoneNumber: yup
       .string()
+      .required("Debe ingresar un número de teléfono")
       .min(8, "Debe tener al menos 8 caracteres")
       .max(15, "No puede superar los 15 caracteres")
       .matches(/^[+0-9\s-]+$/, "Solo se permiten números"),
-    fechaNacimiento: yup
+    birthday: yup
       .date()
-      .nullable()
-      .required("La fecha de nacimiento es obligatoria")
+      .required("Debe ingresar una fecha de nacimiento")
       .max(
         new Date(new Date().setFullYear(new Date().getFullYear() - 10)),
         "Debe tener al menos 10 años"
       ),
-    tipoPago: yup.string().required("Debe seleccionar un tipo de pago"),
-    cantidadDias: yup
+    paymentType: yup.string().required("Debe seleccionar un tipo de pago"),
+    classesPerWeek: yup
       .number()
       .typeError("Debe ingresar un número válido")
       .required("Debe ingresar la cantidad de días")
       .min(1, "Debe ser al menos 1 día")
       .max(7, "No puede superar los 7 días"),
-    fechaIngreso: yup
-      .date()
-      .nullable()
-      .required("La fecha de ingreso es obligatoria"),
-    diaPago: yup.string().when("tipoPago", {
-      is: (val: string) => val === "Día específico",
-      then: (schema) => schema.required("Debe ingresar el día de pago"),
-      otherwise: (schema) => schema.notRequired(),
-    }),
-    diasExtra: yup.string().when("tipoPago", {
-      is: (val: string) => val === "Del 1 al 10",
-      then: (schema) => schema.required("Debe ingresar los días extras"),
-      otherwise: (schema) => schema.notRequired(),
-    }),
+    addmisionDate: yup.date().required("La fecha de ingreso es obligatoria"),
+    payDay: yup
+      .string()
+      .when("paymentType", {
+        is: (val: string) => val === PaymentType.DIA_ESPECIFICO,
+        then: (schema) => schema.required("Debe ingresar el día de pago"),
+        otherwise: (schema) => schema.notRequired(),
+      })
+      .optional(),
+    extraDays: yup
+      .string()
+      .when("paymentType", {
+        is: (val: string) => val === PaymentType.PRINCIPIO_MES,
+        then: (schema) => schema.required("Debe ingresar los días extras"),
+        otherwise: (schema) => schema.notRequired(),
+      })
+      .optional(),
   })
   .required();
