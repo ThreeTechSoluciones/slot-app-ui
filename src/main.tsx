3c40c7967768c211955b25d578641d151ac9f609
fedesan14 <fede14.sanchez@gmail.com>
2025-08-05 19:42:57 -0300

    [FTR][SLOT-37]: Fix error middlware

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ createRoot(document.getElementById('root')!).render(
       <PersistGate persistor={persistor}>
         <BrowserRouter>
           <App />
-          <Toaster />
+          <Toaster position='top-right' />
         </BrowserRouter>
       </PersistGate>
     </Provider>
