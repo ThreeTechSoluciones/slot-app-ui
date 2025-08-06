f452708f1818e532f33443e7364af682a01c3b1b
Federico Sanchez <fede14.sanchez@gmail.com>
2025-08-06 13:35:50 -0300

    [REF][SLOT-37]: add refactor due to code review

--- src/main.tsx
+++ src/main.tsx
@@ -14,7 +14,7 @@ createRoot(document.getElementById('root')!).render(
       <PersistGate persistor={persistor}>
         <BrowserRouter>
           <App />
-          <Toaster position='top-right' />
+          <Toaster />
         </BrowserRouter>
       </PersistGate>
     </Provider>
