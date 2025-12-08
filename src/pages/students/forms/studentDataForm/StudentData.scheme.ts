8a6c0b6e5d7a6b86b27dc06fccbce4acc43b7d35
RocioMenardi <romenardi08@gmail.com>
2025-12-08 20:36:38 -0300

    [ENH][SLOT-91] Add validation for pathologies (no puede tener más de 250 caracteres)

--- src/pages/students/forms/studentDataForm/StudentData.scheme.ts
+++ src/pages/students/forms/studentDataForm/StudentData.scheme.ts
@@ -6,10 +6,8 @@ export const StudentDataScheme = yup.object().shape({
     .required("Debe ingresar el DNI")
     .matches(/^[0-9]+$/, "Solo se permiten números, sin puntos ni espacios")
     .max(8, "DNI no puede tener más de 8 números"),
-  name: yup
-    .string().required("Debe ingresar el nombre"),
-  lastName: yup
-    .string().required("Debe ingresar el apellido"),
+  name: yup.string().required("Debe ingresar el nombre"),
+  lastName: yup.string().required("Debe ingresar el apellido"),
   cellphoneNumber: yup
     .string()
     .required("Debe ingresar un número de teléfono")
@@ -17,22 +15,26 @@ export const StudentDataScheme = yup.object().shape({
     .max(11, "No puede tener más de 11 caracteres")
     .matches(/^[0-9]+$/, "Solo se permiten números, sin espacios"),
   pathologies: yup
-    .string().notRequired().default(null),
+    .string()
+    .notRequired()
+    .default(null)
+    .max(250, "No puede tener más de 250 caracteres"),
   birthday: yup
     .string()
     .required("Debe ingresar una fecha de nacimiento")
-    .test('fecha-valida', 'La fecha no puede ser mayor a la actual', (value) => {
-      if (!value) return false;
-      const fechaIngresada = new Date(value);
-      const hoy = new Date();
-      hoy.setHours(0, 0, 0, 0);
-      return fechaIngresada <= hoy;
-    })
+    .test(
+      "fecha-valida",
+      "La fecha no puede ser mayor a la actual",
+      (value) => {
+        if (!value) return false;
+        const fechaIngresada = new Date(value);
+        const hoy = new Date();
+        hoy.setHours(0, 0, 0, 0);
+        return fechaIngresada <= hoy;
+      }
+    )
     // Esto evita error si el campo está vacío
     .transform((value, originalValue) => {
       return originalValue === "" ? null : value;
     }),
-
 });
-
-
