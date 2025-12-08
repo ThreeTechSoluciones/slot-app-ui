b4b8b894f7f5c96f324f6a397a234ff7deade7ce
AbrilConrero <136394699+AbrilConrero@users.noreply.github.com>
2025-12-08 12:54:19 -0300

    [FTR][SLOT-104]: Resolviendo conflictos

--- src/pages/slots/SlotConfiguration.styles.ts
+++ src/pages/slots/SlotConfiguration.styles.ts
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import styled, { keyframes } from "styled-components";
 import { BORDER_RADIUS, FONT_FAMILY, TERTIARY_COLOR, PRIMARY_COLOR, TEXT_COLOR, SECONDARY_COLOR, FONT_WEIGHT_BOLD, FONT_WEIGHT_NORMAL, BACKGROUND_COLOR } from "../../utils/Stylesheet";
 import Arrow from "../../assets/Arrow.png";
@@ -24,12 +23,6 @@ export const SkeletonsContainer = styled.div`
     align-items: flex-start; 
     gap: 32px;
 `;
-=======
-import styled from "styled-components";
-import { BORDER_RADIUS, FONT_FAMILY, TERTIARY_COLOR, PRIMARY_COLOR, TEXT_COLOR, SECONDARY_COLOR } from "../../utils/Stylesheet";
-import Arrow from "../../assets/Arrow.png";
-
->>>>>>> dev
 
 export const ScreenContainer = styled.div`
     display: flex;
@@ -40,24 +33,9 @@ export const ScreenContainer = styled.div`
     padding-bottom: 32px;
     gap:24px;
     border: 2px solid ${TERTIARY_COLOR};
-<<<<<<< HEAD
     border-radius: ${BORDER_RADIUS}; 
 `;
 
-=======
-    border-radius: ${BORDER_RADIUS};
-`;
-
-export const MainContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 100%;
-    min-height: 100vh;
-
-`
-
->>>>>>> dev
 export const InputContainer = styled.div`
     display: flex;  
     flex-direction: column;
@@ -72,12 +50,7 @@ export const EditContainer = styled.div`
 `;
 export const Label = styled.label`
     font-size: 16px;
-<<<<<<< HEAD
     font-family: ${FONT_FAMILY};  
-=======
-    font-family: ${FONT_FAMILY};
-   
->>>>>>> dev
 `;
 
 export const BaseStyle = styled.input`
@@ -110,10 +83,6 @@ export const Select = styled(BaseStyle).attrs({ as: "select" })`
 
 interface ButtonProps {
     $isDisabled?: boolean;
-<<<<<<< HEAD
-
-=======
->>>>>>> dev
 }
 
 export const Button = styled.button<ButtonProps>`
