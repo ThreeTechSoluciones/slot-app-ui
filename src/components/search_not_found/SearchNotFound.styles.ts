b2dc20d0c688369205d0e6f6024bf2e0ce8843bf
LuciaRosales9 <luciarosalesb9@gmail.com>
2026-01-08 22:32:44 -0300

    [ENH][SLOT-124]: alignment and color adjustment

--- src/components/search_not_found/SearchNotFound.styles.ts
+++ src/components/search_not_found/SearchNotFound.styles.ts
@@ -1,21 +1,21 @@
 import styled from "styled-components";
 
 export const Container = styled.div`
-  margin-top: 40px;
   display: flex;
   flex-direction: column;
   align-items: center;
-  gap: 16px;
+  gap: 2px;
+  margin-top: 60px;
 `;
 
 export const Icon = styled.img`
-  width: 64px;
-  height: 64px;
+  width: 40px;
+  height: 40px;
 `;
 
 export const Message = styled.p`
   text-align: center;
-  color: #9ca3af;
+  color: #000;
   font-size: 14px;
-  max-width: 360px;
+  max-width: 460px;
 `;
