1ca49e0bc2b11ec02257e295e0414dd03872d0c8
RocioMenardi <romenardi08@gmail.com>
2026-01-24 12:03:15 -0300

    [FTR][SLOT-113] Remove unused imports

--- src/app/services/UserService.ts
+++ src/app/services/UserService.ts
@@ -4,10 +4,8 @@ import type { PriceResponse } from "../types/responses/PriceResponse.type";
 import type { PlanResponse } from "../types/responses/PlanResponse.type";
 import type { Page } from "../types/responses/common/Page";
 import type { SlotListResponse } from "../types/responses/SlotResponse.type";
-import type { GetSlotsByDayParams } from "../types/requests/GetUserSlotsRequest.type";
 import type { SortConfig } from "../types/sort";
 import type { UserPreferencesResponse } from "../types/responses/UserPreferencesResponse.type";
-import type { CalendarResponse } from "../types/responses/CalendarResponse.type";
 
 export const UserService = createApi({
   reducerPath: "users",
