55fb8d106774edaf05958ceb6bc5c69d7928abb6
RocioMenardi <romenardi08@gmail.com>
2026-02-20 18:46:07 -0300

    [FIX][SLOT-224] Improve widths

--- src/pages/students/forms/paymentDataForm/PaymentData.styles.ts
+++ src/pages/students/forms/paymentDataForm/PaymentData.styles.ts
@@ -6,7 +6,7 @@ import {
   DEFAULT_TEXT_COLOR,
   FONT_FAMILY,
 } from "../../../../utils/Stylesheet";
-
+export const FORM_WIDTH = "410px";
 export const MainContainer = styled.div`
   display: flex;
   flex-direction: column;
@@ -15,10 +15,16 @@ export const MainContainer = styled.div`
 `;
 
 export const FormContainer = styled.form`
+  max-width: ${FORM_WIDTH};
   display: flex;
   flex-direction: column;
   align-items: center;
   width: 100%;
+`;
+export const FieldContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
 `;
 export const Label = styled.p`
   font-size: 16px;
@@ -28,7 +34,7 @@ export const Label = styled.p`
   font-family: ${FONT_FAMILY};
 `;
 export const BaseStyle = styled.input`
-  width: 410px;
+  width: 100%;
   height: 56px;
   border: 1px solid black;
   border-radius: 10px;
@@ -43,6 +49,7 @@ export const BaseStyle = styled.input`
   }
 `;
 export const Select = styled(BaseStyle).attrs({ as: "select" })`
+  width: 100%;
   height: 56px;
   border-radius: 10px;
   border: 1px solid black;
@@ -55,11 +62,9 @@ export const Select = styled(BaseStyle).attrs({ as: "select" })`
   background: url(${Arrow}) no-repeat right 12px center;
   padding-right: 32px;
 `;
-interface InputProps {
-  $isSmallSize?: boolean;
-}
-export const Input = styled(BaseStyle)<InputProps>`
-  width: ${(props) => (props.$isSmallSize ? "196px" : "410px")};
+
+export const Input = styled(BaseStyle)`
+  width: 100%;
   background: ${(props) => (props.disabled ? LIGHT_NEUTRAL_COLOR : "none")};
   border: ${(props) =>
     props.disabled ? LIGHT_NEUTRAL_COLOR : "1px solid black"};
@@ -70,7 +75,7 @@ export const Input = styled(BaseStyle)<InputProps>`
 export const InputsContainer = styled.div`
   display: flex;
   flex-direction: column;
-  align-items: center;
+  width: 100%;
 `;
 
 interface TextProps {
@@ -84,11 +89,10 @@ export const Text = styled.p<TextProps>`
   margin-top: ${(props) => (props.$isRegister ? "8px" : "0px")};
   margin-bottom: ${(props) => (props.$isRegister ? "8px" : "0px")};
   width: 100%;
-  max-width: 410px;
 `;
 export const SecondaryInputsContainer = styled.div`
   display: flex;
-  flex-direction: row;
+  align-items: flex-start;
   gap: 18px;
-  width: 410px;
+  width: 100%;
 `;
