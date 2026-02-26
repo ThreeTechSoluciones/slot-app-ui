cfac33896ce95768131cf114c8fde26c6ff6d699
RocioMenardi <romenardi08@gmail.com>
2026-02-26 11:50:39 -0300

    [FIX][SLOT-250] Add max limit

--- src/pages/slots/forms/EditCapacity.scheme.ts
+++ src/pages/slots/forms/EditCapacity.scheme.ts
@@ -5,5 +5,6 @@ export const EditCapacityScheme = yup.object().shape({
     .number()
     .required('Debe ingresar un número')
     .typeError('Debe ingresar un número')
-    .min(1, 'El cupo debe ser al menos 1'),
+    .min(1, 'El cupo debe ser al menos 1')
+    .max(127, 'El cupo no puede exceder 127'),
 });
