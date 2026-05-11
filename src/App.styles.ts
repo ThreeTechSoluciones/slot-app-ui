87d57b0cbefb2ea8d0a54760b0c7c5b790935282
AbrilConrero <136394699+AbrilConrero@users.noreply.github.com>
2026-05-11 01:25:20 -0300

    [ENH][SLOT-303]: Implement code review feedback

--- src/App.styles.ts
+++ src/App.styles.ts
@@ -1,6 +1,7 @@
 import { styled } from 'styled-components';
+import { HEIGHT_HEADER } from './components/header/header.styles';
 
 export const PrivateContent = styled.div`
-  margin-top: 110px;
+  margin-top: ${HEIGHT_HEADER};
 `;
 export const MainContent = styled.main``;
