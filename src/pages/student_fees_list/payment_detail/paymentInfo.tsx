c464152129427f578c576fafaf15834ec5eb7de2
Fedesan14 <fede14.sanchez@gmail.com>
2026-05-28 08:52:44 -0300

    Resolve conflicts

--- src/pages/student_fees_list/payment_detail/paymentInfo.tsx
+++ src/pages/student_fees_list/payment_detail/paymentInfo.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
 import {
-  ModalOverlay,
-  ModalContainer,
-  ModalHeader,
-  ModalTitle,
-  FeeTitle,
+  PaymentInfoContainer,
   TextColumn,
   FieldLabel,
   Value,
   ModalContent,
   ModalRow,
-  CloseButton,
   IconCircle,
-} from './paymentInfo.styles';
-import CancelIcon from '../../../assets/cancel-icon.svg';
+  MonthlyFeeText,
+} from './PaymentInfo.styles';
 import HashtagIcon from '../../../assets/hashtag-icon.svg';
 import PesoIcon from '../../../assets/peso-icon.svg';
 import CalendarIcon from '../../../assets/calendar-icon.svg';
@@ -22,80 +17,64 @@ import { useGetPaymentInfoQuery } from '../../../app/services/PaymentService';
 import { skipToken } from '@reduxjs/toolkit/query';
 
 interface PaymentInfoModalProps {
-  isOpen: boolean;
-  onClose: () => void;
   paymentId: string | null;
 }
 
-const PaymentInfoModal: React.FC<PaymentInfoModalProps> = ({ isOpen, onClose, paymentId }) => {
+const PaymentInfoModal: React.FC<PaymentInfoModalProps> = ({ paymentId }) => {
   const { data: payment, isLoading, isError } = useGetPaymentInfoQuery(paymentId ?? skipToken);
-  if (!isOpen || !paymentId) return null;
+  if (!paymentId) return null;
+
   if (isLoading) {
     return (
-      <ModalOverlay>
-        <ModalContainer>
-          <div>Cargando información del pago...</div>
-        </ModalContainer>
-      </ModalOverlay>
+      <PaymentInfoContainer>
+        <div>Cargando información del pago...</div>
+      </PaymentInfoContainer>
     );
   }
+
   if (isError || !payment) {
     return (
-      <ModalOverlay>
-        <ModalContainer>
-          <CloseButton onClick={onClose}>
-            <img src={CancelIcon} alt="Close" />
-          </CloseButton>
-          <div>Error al cargar la información del pago</div>
-        </ModalContainer>
-      </ModalOverlay>
+      <PaymentInfoContainer>
+        <div>Error al cargar la información del pago</div>
+      </PaymentInfoContainer>
     );
   }
+
   return (
-    <ModalOverlay>
-      <ModalContainer>
-        <CloseButton onClick={onClose}>
-          <img src={CancelIcon} alt="Close" />
-        </CloseButton>
-        <ModalHeader>
-          <ModalTitle>DETALLE DEL PAGO</ModalTitle>
-        </ModalHeader>
+    <PaymentInfoContainer>
+      <MonthlyFeeText>{`#Cuota N°${payment.monthlyFeeNumber}`}</MonthlyFeeText>
+      <ModalContent>
+        <ModalRow>
+          <IconCircle>
+            <img src={HashtagIcon} alt="Número de pago" width={16} height={16} />
+          </IconCircle>
+          <TextColumn>
+            <FieldLabel>N° de pago </FieldLabel>
+            <Value>{payment.paymentNumber}</Value>
+          </TextColumn>
+        </ModalRow>
+        <ModalRow>
+          <IconCircle>
+            <img src={PesoIcon} alt="Signo Pesos" width={11} height={19} />
+          </IconCircle>
 
-        <FeeTitle>#Cuota N°{payment.monthlyFeeNumber}</FeeTitle>
+          <TextColumn>
+            <FieldLabel>Monto </FieldLabel>
+            <Value>{formatCurrency(payment.amount)}</Value>
+          </TextColumn>
+        </ModalRow>
+        <ModalRow>
+          <IconCircle>
+            <img src={CalendarIcon} alt="Calendario" width={16} height={16} />
+          </IconCircle>
 
-        <ModalContent>
-          <ModalRow>
-            <IconCircle>
-              <img src={HashtagIcon} alt="Número de pago" width={16} height={16} />
-            </IconCircle>
-            <TextColumn>
-              <FieldLabel>N° de pago </FieldLabel>
-              <Value>{payment.paymentNumber}</Value>
-            </TextColumn>
-          </ModalRow>
-          <ModalRow>
-            <IconCircle>
-              <img src={PesoIcon} alt="Signo Pesos" width={11} height={19} />
-            </IconCircle>
-
-            <TextColumn>
-              <FieldLabel>Monto </FieldLabel>
-              <Value>{formatCurrency(payment.amount)}</Value>
-            </TextColumn>
-          </ModalRow>
-          <ModalRow>
-            <IconCircle>
-              <img src={CalendarIcon} alt="Calendario" width={16} height={16} />
-            </IconCircle>
-
-            <TextColumn>
-              <FieldLabel>Fecha de pago</FieldLabel>
-              <Value> {payment.paymentDate}</Value>
-            </TextColumn>
-          </ModalRow>
-        </ModalContent>
-      </ModalContainer>
-    </ModalOverlay>
+          <TextColumn>
+            <FieldLabel>Fecha de pago</FieldLabel>
+            <Value> {payment.paymentDate}</Value>
+          </TextColumn>
+        </ModalRow>
+      </ModalContent>
+    </PaymentInfoContainer>
   );
 };
 
